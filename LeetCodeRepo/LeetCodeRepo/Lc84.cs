735d88f9e2a161902b3d03f4b8ed560c9d5aa02d
Peter <sddlpeter@gmail.com>
2020-01-19 11:54:18 -0800

    c-0120

--- LeetCodeRepo/LeetCodeRepo/Lc84.cs
+++ LeetCodeRepo/LeetCodeRepo/Lc84.cs
@@ -12,4 +12,39 @@ namespace LeetCodeRepo{
             return maxarea;
         }
     }
+
+    public class Lc84_II{
+        public int LargestRectangleArea(int[] heights){
+            return calculateArea(heights, 0, heights.Length -1);
+        }
+
+        public int calculateArea(int[] heights, int start, int end){
+            if(start>end) return 0;
+            int minindex = start;
+            for(int i = start; i<=end; i++){
+                if(heights[minindex] > heights[i]){
+                    minindex = i;
+                }
+            }
+            return Math.Max(heights[minindex] * (end - start +1), Math.Max(calculateArea(heights, start, minindex -1), calculateArea(heights, minindex+1, end)));
+        }
+    }
+
+    public class Lc84_V{
+        public int LargestRectangleArea(int[] heights){
+            Stack<int> stack = new Stack<int>();
+            stack.Push(-1);
+            int maxarea = 0;
+            for(int i = 0; i<heights.Length; i++){
+                while(stack.Peek() != -1 && heights[stack.Peek()] >= heights[i]){
+                    maxarea = Math.Max(maxarea, heights[stack.Pop()] * (i-stack.Peek() -1));
+                }
+                stack.Push(i);
+            }
+            while(stack.Peek()!=-1){
+                maxarea = Math.Max(maxarea, heights[stack.Pop()] * (heights.Length- stack.Peek() -1));
+            }
+            return maxarea;
+        }
+    }
 }
